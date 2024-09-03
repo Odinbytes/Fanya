8e54dbdfcb00f32aa02635c1ba20a098722de923
Aw3som3ss <jabwunza@gmail.com>
2024-09-03 13:07:54 +0300

    UI

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import todo_icon from '../assets/todo_icon.png'
+import TodoItems from './TodoItems'
 
 const Todo = () => {
   return (
@@ -17,11 +18,18 @@ const Todo = () => {
         </div>
 
 
-    {/* input box */}
+    {/* input box  */}
 
         <div className='flex items-center my-3 bg-yellow-200 rounded-full'>
             <input className='bg-transparent border-0 outline-none flex-1 h-10 pl-6 pr-3 placeholder:text-slate-600' type='text' placeholder='Add your task'/>
             <button className='border-none rounded-full bg-orange-600 w-32 h-10 text-white'>ADD +</button>
+        </div>
+
+    {/* list of added items */}
+
+        <div>
+            <TodoItems text="Learn coding"/>
+            <TodoItems text="Learn backend"/>
         </div>
 
     </div>
