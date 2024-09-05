250ee098e044215a5ab6fe9382cbe98cff0ca19f
Aw3som3ss <jabwunza@gmail.com>
2024-09-05 18:24:43 +0300

    toggle item functionality(done or not done)

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import tick from '../assets/tick.png'
-// import not_tick from '../assets.not_tick,png'
+import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 
-const TodoItems = ({text, id, isComplete, deleteTodo}) => {
+const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1 items-center cursor-pointer'>
-            <img src={tick} alt="" className='w-7'/>
+        <div onClick={() => {toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
+            <img src={isComplete ? tick : not_tick} alt="" className='w-7'/>
             <p className='text-slate-900 ml-4 text-[20px]'>{text}</p>
         </div>
             <img onClick={() => {deleteTodo(id)}} src={delete_icon} alt="" className='w-5 cursor-pointer'/>
